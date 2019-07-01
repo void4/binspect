1b496cf2a0bbfaeae9f5a7208efe879a7e09c4be
Noreply <noreply@github.com>
2019-07-01 18:31:58 +0200

    Support single file checks

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from collections import defaultdict
 from operator import lt, gt, le, ge, eq, ne
 from utils import progressbar
 import traceback
+import struct
 
 files = []
 
+# TODO support glob *.sav, sorted
 filepaths = sys.argv[1:]
 
 if len(filepaths) < 2:
@@ -15,7 +17,14 @@ if len(filepaths) < 2:
 for path in filepaths:
 	files.append(open(path, "rb"))
 
-data = [f.read() for f in files]
+data = []
+for f in files:
+	data.append([])
+	while True:
+		try: 
+			data[-1].append(struct.unpack_from("<H",f.read(4))[0])
+		except struct.error:
+			break
 
 opmap = {
 	"=":eq,
@@ -38,7 +47,7 @@ class Filter:
 	
 	def reset(self):
 		print("Reset table")
-		self.kv = defaultdict(list)
+		self.kv = defaultdict(dict)
 
 	def getByte(self, fileindex, byteindex):
 			if fileindex==0:
@@ -67,7 +76,6 @@ class Filter:
 			else:
 				bytelist = [(i,b) for i,b in enumerate(filedata) if i in self.kv]
 			for byteindex, byte in progressbar(bytelist, filepaths[fileindex]+": ", 40):
-				#print("HERE", byteindex)
 				previousbyte = self.getByte(fileindices[indexno], byteindex)
 				if previousbyte is None:
 					#delete
@@ -83,29 +91,47 @@ class Filter:
 							del self.kv[byteindex]
 			
 			print(len(self.kv))
-			
+	
+	def print(self):
+		for k,v in self.kv.items():
+			print(k,hex(k),v)
 			
 	def readall(self):
 		print("Readall")
-		for d in data:
+		for di, d in enumerate(data):
 			for bi, b in enumerate(d):
-				self.kv[bi].append(b)#no space for nonexistent data currently! (e.g. file 1 shorter than 0 and 2)
+				self.kv[bi][di] = b
+				#self.kv[bi].append(b)#no space for nonexistent data currently! (e.g. file 1 shorter than 0 and 2)
 
-	def filter(self, fileindices, condition, value):
+	def filter(self, fileindices, condition, target):
 	
 		if len(self.kv) == 0:
 			self.readall()
 	
 		filtered = 0
+		nonexistent = 0
+		falsecond = 0
+		
+
 		for key in list(self.kv.keys()):
 			value = self.kv[key]
-			#BE CAUTIOUS, kv value only includes CHECKED indices, may omit some, like 1 if user just typed 0,2 <
-			#FOR NOW, assume user checked all files
-			if not condition(value[fileindex], value):
-				del self.kv[key]
-				filtered += 1
+			for fileindex in fileindices:
+				
+				delete = False
+				if not fileindex in value:
+					delete = True
+					nonexistent += 1
+					
+				
+				elif not condition(value[fileindex], target):
+					delete = True
+					falsecond += 1
+					
+				if delete:
+					del self.kv[key]
+					filtered += 1
 		
-		print("Filtered: ", filtered)
+		print(f"Filtered: {filtered} (Falsecond: {falsecond} | Nonexistent: {nonexistent})")
 			
 	def step(self):
 		print(len(self.kv))
@@ -126,14 +152,17 @@ class Filter:
 		if len(cmd) == 0:
 			return
 		elif len(cmd) == 1:
-			if cmd[0] in opmap:
+			cmd = cmd[0]
+			if cmd in opmap:
 				#new, then old
-				condition = opmap[cmd[0]]
+				condition = opmap[cmd]
 				self.filecmp(list(range(len(filepaths))), condition)
-			elif cmd[0] in "q exit quit".split():
+			elif cmd in "q exit quit".split():
 				exit(0)
-			elif cmd[0] == "reset":
+			elif cmd in "r reset clear".split():
 				self.reset()
+			elif cmd in "o p print out show".split():
+				self.print()
 			else:
 				raise Exception("Unknown operator", cmd[0])
 		elif len(cmd) == 2:
@@ -149,7 +178,7 @@ class Filter:
 		elif len(cmd) == 3:
 			fileindices = [int(i) for i in cmd[0].split(",")]
 			condition = getop(cmd[1])
-			value = cmd[2]
+			value = int(cmd[2])
 			self.filter(fileindices, condition, value)
 		else:
 			raise Exception("Unknown command", cmd)				
